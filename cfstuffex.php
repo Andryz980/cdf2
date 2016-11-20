65fe818f4a5a41e55e48d5baab63aa56307e1216
Andryz980 <andryboe@gmail.com>
2016-11-20 15:27:49 +0100

    It actually works

--- cfstuffex.php
+++ cfstuffex.php
@@ -1,6 +1,6 @@
 <?php
 
-    if (isset($_POST['sub']))
+    if (isset($_POST['sub'])) //When data is submitted by the button
     {
         $name = $_POST["name"]; 
         $surname = $_POST["surname"]; 
@@ -8,14 +8,13 @@
         $gen = $_POST["gen"]; 
         $city = $_POST["city"]; 
         
-        echo call($name, $surname, $bday, $gen, $city);
+        echo callofthewild($name, $surname, $bday, $gen, $city);
     }
 
-function call($name, $surname, $bday, $gen, $city)
+function callofthewild($name, $surname, $bday, $gen, $city)
 {
     $tmp = new cf;
-    return $tmp->calcola($name, $surname, $bday, $gen, $city);
-    
+    return $tmp->startdotexe($name, $surname, $bday, $gen, $city);
 }
 
 class cf
@@ -35,21 +34,21 @@ class cf
 
     protected $_odds = array('0' =>  1, '1' =>  0, '2' =>  5, '3' =>  7, '4' =>  9, '5' => 13, '6' => 15, '7' => 17, '8' => 19, '9' => 21, 'A' =>  1, 'B' =>  0, 'C' =>  5, 'D' =>  7, 'E' =>  9, 'F' => 13, 'G' => 15, 'H' => 17, 'I' => 19, 'J' => 21, 'K' =>  2, 'L' =>  4, 'M' => 18, 'N' => 20, 'O' => 11, 'P' =>  3, 'Q' =>  6, 'R' =>  8, 'S' => 12, 'T' => 14, 'U' => 16, 'V' => 10, 'W' => 22, 'X' => 25, 'Y' => 24, 'Z' => 23);
 
-    protected $_controllo = array('0'  => 'A', '1'  => 'B', '2'  => 'C', '3'  => 'D', '4'  => 'E', '5'  => 'F', '6'  => 'G', '7'  => 'H', '8'  => 'I', '9'  => 'J', '10' => 'K', '11' => 'L', '12' => 'M', '13' => 'N', '14' => 'O', '15' => 'P', '16' => 'Q', '17' => 'R', '18' => 'S', '19' => 'T', '20' => 'U', '21' => 'V', '22' => 'W', '23' => 'X', '24' => 'Y', '25' => 'Z');    
+    protected $_last = array('0'  => 'A', '1'  => 'B', '2'  => 'C', '3'  => 'D', '4'  => 'E', '5'  => 'F', '6'  => 'G', '7'  => 'H', '8'  => 'I', '9'  => 'J', '10' => 'K', '11' => 'L', '12' => 'M', '13' => 'N', '14' => 'O', '15' => 'P', '16' => 'Q', '17' => 'R', '18' => 'S', '19' => 'T', '20' => 'U', '21' => 'V', '22' => 'W', '23' => 'X', '24' => 'Y', '25' => 'Z');    
     //These three arrays will come in handy in the last char function.
     
 
-    protected function _getl($string, array $haystack) //This function returns either the vowels or the consonants of a string.
+    protected function _getl($string, array $group) //This function returns either the vowels or the consonants of a string.
     { 
         $letters = array();
-        foreach(str_split($string) as $needle) //The string is split and then every char is checked.
+        foreach(str_split($string) as $single) //The string is split and then every char is checked.
         {
-            if (in_array($needle, $haystack)) //in_array checks if a letter is present in the group I specified, either vowels or consonants.
+            if (in_array($single, $group)) //in_array checks if a letter is present in the group I specified, either vowels or consonants.
             {
-                $letters[] = $needle;
+                $new[] = $single;
             }
         }
-        return $letters;
+        return $new;
     }
 
     protected function _getvows($string) //This function calls the getl function with consonants...
@@ -62,7 +61,7 @@ class cf
         return $this->_getl($string, $this->_cons);
     }
 
-    protected function _addMissingX($string) //This function adds some X if the string is shorter than 3 chars.
+    protected function _addx($string) //This function adds some X if the string is shorter than 3 chars.
     {
         $code = $string;
         
@@ -73,16 +72,17 @@ class cf
         return $code;
     }
     
-    protected function _calcolaNome($nome)
+    protected function _calcolaNome($input) //OKAY
     //This function returns the first, third and fourth consonants (adding vowels if shorter than 3) of the name.
     {
-
+        $nome = strtoupper($input);
+        
         if (strlen($nome) < 3)
         {
-            return $this->_addMissingX($nome);
-        } 
+            return $this->_addx($nome);
+        }
         
-        $nome_cons = $this->_getcons($nome);
+        $ncons = $this->_getcons($nome);
 
         if (count($ncons) <= 3)
         {
@@ -105,26 +105,30 @@ class cf
         if (strlen($code) < 3) 
         {
             $nvoc = $this->_getvows($nome);
-            while (strlen($code) < 3)
+            //let me try
+            $i = 0;
+            while (strlen($code) < 3 && $i < count($nvoc))
             {
-               $code = $code . array_shift($nvoc); 
+                $code = $code . $nvoc[$i];
+                $i++;
             }
         }
-        
+        //echo $code;
         return $code;
     }
 
-    protected function _calcolaCognome($cognome)
+    protected function _calcolaCognome($input) //OKAY
     //This function returns the first three consonants (adding vowels if shorter than 3) of the name.
     {
-
+        $cognome = strtoupper($input);
         if (strlen($cognome) < 3)
         {
-            return $this->_addMissingX($cognome);
+            return $this->_addx($cognome);
         }
 
         $ccons = $this->_getcons($cognome);
         
+        $code= "";
         
         for ($i=0; $i<3; $i++) 
         {
@@ -137,16 +141,19 @@ class cf
         if (strlen($code) < 3)
         {
             $cvoc = $this->_getvows($cognome);
-            while (strlen($code) < 3)
+            $i = 0;
+            while (strlen($code) < 3 && $i < count($cvoc))
             {
-                $code = $code . array_shift($cvoc);
+                $code = $code . $cvoc[$i];
+                $i++;
+                echo $i;
             }
         }
-
+        //echo $code;
         return $code;   
     }
 
-    protected function _calcolaDataNascita($data, $gen)
+    protected function _calcolaDataNascita($data, $gen) //OKAY
     //This function returns the five symbols after the six ones. These symbols are related to a person's date of birth and gender.
     {
         $dn = explode('/', $data); //Separates the "01/01/1900" format in an array containing the single elements.
@@ -155,8 +162,11 @@ class cf
         $mese   = (int) @$dn[1];
         $anno   = (int) @$dn[2];
         
-        $aa = substr($anno, -2); //Removes the first two numbers from the year.
-        
+        if ($anno > 1000)
+            $aa = substr($anno, -2); //Removes the first two numbers from the year.
+        else
+            $aa = $anno; //if the user inputs XX instead if XXXX, the number is already right.
+            
         $mm = $this->_mesi[$mese]; //Fetchs the letter associated with the person's month.
         
         if ($gen != "Male")
@@ -165,48 +175,42 @@ class cf
             $gg = $giorno;
         
         
-        if (strlen($gg) < 2) $gg = '0' . $gg;
-
+        if (strlen($gg) < 2)
+            $gg = '0' . $gg;
+        
         return $aa . $mm . $gg;
     }
 
-    protected function _calcolaCatastale($comune)
+    protected function _calcolaComune($comune)
     {
         return "L840"; //WIP
     }
 
-    protected function _calcolaCifraControllo($codice)
+    protected function _ccontrollo($codice)
     {
         $code = str_split($codice);
         $sum  = 0;
-
+        $cifra = 0;
+        
         for($i=1; $i <= count($code); $i++)
         {
             $cifra = $code[$i-1];
-            $sum += ($i % 2) ? $this->_odds[$cifra] : $this->_even[$cifra];
+            $sum += ($i % 2) ? $this->_odds[$cifra] : $this->_even[$cifra]; 
+            //This was used to avoid all the "if" list we used in the first javascript code.
         }
 
         $sum %= 26;
 
-        return $this->_controllo[$sum];
+        return $this->_last[$sum];
     }
 
-    public function calcola($nome, $cognome, $data, $sesso, $comune)
+    public function startdotexe($name, $surname, $bday, $gen, $city)
     {
-        $codice = $this->_calcolaCognome($cognome) . 
-                  $this->_calcolaNome($nome) . 
-                  $this->_calcolaDataNascita($data, $sesso) . 
-                  $this->_calcolaCatastale($comune);
-
-        
+        $codice = $this->_calcolaCognome($surname) . 
+                  $this->_calcolaNome($name) . 
+                  $this->_calcolaDataNascita($bday, $gen) . 
+                  $this->_calcolaComune($city);
  
-        $codice .= $this->_calcolaCifraControllo($codice);
-
-        if (strlen($codice) != 16)
-        {
-            return false;
-        }
-
-        return $codice;
+        return $codice = $codice . $this->_ccontrollo($codice);
     } 
 }
