115415f1d913f2d63541b2df05e201b38685e868
Andryz980 <andryboe@gmail.com>
2016-11-22 08:45:30 +0100

    Should!

--- index.php
+++ index.php
@@ -94,9 +94,8 @@ function callofthewild($name, $surname, $bday, $gen, $city) //Start.
      
 function finalprint($output)
 {
-      echo "<div class="col-xs-12>$output</div>";
+      echo "<div class="col-xs-12">$output</div>";
 }
-     
      
 class cf
 {
